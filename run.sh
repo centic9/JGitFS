ef77255c850b468586e2ffa982aab06123517ad8
Dominik Stadler <dominik.stadler@gmx.at>
2013-06-17 18:10:18 +0200

    Adjust for changes in fuse-jna

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp bin:lib/jna.jar:lib/org.eclipse.jgit-2.3.1.201302201838-r.jar:../fuse-jna/bin  org.dstadler.jgitfs.JGitFS $*
+java -cp bin:../fuse-jna/lib/jna/jna-3.5.2.jar:lib/org.eclipse.jgit-2.3.1.201302201838-r.jar:../fuse-jna/bin  org.dstadler.jgitfs.JGitFS $*
