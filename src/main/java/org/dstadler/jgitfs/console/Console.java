5132b58f9beb57f0c60f54376e863746be4949ae
Dominik Stadler <dominik.stadler@gmx.at>
2016-06-28 15:34:37 +0200

    Update third party libs: commons-io, jgit, jline, slf4j, junit, jacoco Adjust for Closeable ConsoleReader

--- src/main/java/org/dstadler/jgitfs/console/Console.java
+++ src/main/java/org/dstadler/jgitfs/console/Console.java
@@ -22,65 +22,65 @@ import org.dstadler.jgitfs.JGitFS;
 public class Console {
 
     public void run(final InputStream inStream, final OutputStream outStream) throws IOException {
-        ConsoleReader reader = new ConsoleReader("JGitFS", inStream, outStream, null);
+        try (ConsoleReader reader = new ConsoleReader("JGitFS", inStream, outStream, null)) {
+            reader.setPrompt("jgitfs> ");
 
-        reader.setPrompt("jgitfs> ");
+            reader.addCompleter(new FileNameCompleter());
+            reader.addCompleter(new StringsCompleter(Arrays.asList(new String[]{
+                    "mount",
+                    "unmount",
+                    "list",
+                    "exit",
+                    "quit",
+                    "cls",
+            })));
 
-        reader.addCompleter(new FileNameCompleter());
-        reader.addCompleter(new StringsCompleter(Arrays.asList(new String[] {
-                "mount",
-                "unmount",
-                "list",
-                "exit",
-                "quit",
-                "cls",
-        })));
+            // TODO: the completers do not seem to work, is there more to do to make them work?
 
-        // TODO: the completers do not seem to work, is there more to do to make them work?
+            String line;
+            PrintWriter out = new PrintWriter(reader.getOutput());
 
-        String line;
-        PrintWriter out = new PrintWriter(reader.getOutput());
-
-        while ((line = reader.readLine()) != null) {
-            if (line.startsWith("mount")) {
-                String[] cmd = line.split("\\s+");
-                if(cmd.length < 3) {
-                    out.println("Invalid command");
-                    help(out);
-                } else {
-                    //out.println("Mounting " + cmd[1] + " at " + cmd[2]);
-                    try {
-                        JGitFS.mount(cmd[1], new File(cmd[2]));
-                    } catch (IllegalArgumentException | IllegalStateException | IOException | FuseException e) {
-                        e.printStackTrace(out);
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("mount")) {
+                    String[] cmd = line.split("\\s+");
+                    if (cmd.length < 3) {
+                        out.println("Invalid command");
+                        help(out);
+                    } else {
+                        //out.println("Mounting " + cmd[1] + " at " + cmd[2]);
+                        try {
+                            JGitFS.mount(cmd[1], new File(cmd[2]));
+                        } catch (IllegalArgumentException | IllegalStateException | IOException | FuseException e) {
+                            e.printStackTrace(out);
+                        }
                     }
-                }
-            } else if (line.startsWith("unmount") || line.startsWith("umount")) {
-                String[] cmd = line.split("\\s+");
-                if(cmd.length < 2) {
-                    out.println("Invalid command");
-                    help(out);
-                } else {
-                    //out.println("Umounting " + cmd[1]);
-                    try {
-                        JGitFS.unmount(cmd[1]);
-                    } catch (IOException e) {
-                        e.printStackTrace(out);
+                } else if (line.startsWith("unmount") || line.startsWith("umount")) {
+                    String[] cmd = line.split("\\s+");
+                    if (cmd.length < 2) {
+                        out.println("Invalid command");
+                        help(out);
+                    } else {
+                        //out.println("Umounting " + cmd[1]);
+                        try {
+                            JGitFS.unmount(cmd[1]);
+                        } catch (IOException e) {
+                            e.printStackTrace(out);
+                        }
                     }
+                } else if (line.startsWith("list")) {
+                    JGitFS.list();
+                } else if (line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("exit")) {
+                    break;
+                } else if (line.equalsIgnoreCase("cls")) {
+                    reader.clearScreen();
+                } else {
+                    help(out);
                 }
-            } else if (line.startsWith("list")) {
-                JGitFS.list();
-            } else if (line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("exit")) {
-                break;
-            } else if (line.equalsIgnoreCase("cls")) {
-                reader.clearScreen();
-            } else {
-                help(out);
             }
-        }
 
-        // ensure that all content is written to the screen at the end to make unit tests stable
-        reader.flush();
+            // ensure that all content is written to the screen at the end to make unit tests stable
+            reader.flush();
+        }
     }
 
     private void help(PrintWriter out) {
