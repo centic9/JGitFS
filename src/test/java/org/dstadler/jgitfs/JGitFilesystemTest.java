3ba7483603701502fd0758757801d8a06bc47cd3
Dominik Stadler <dominik.stadler@gmx.at>
2017-06-16 20:07:45 +0200

    Try to make test run both locally and in Travis-CI

--- src/test/java/org/dstadler/jgitfs/JGitFilesystemTest.java
+++ src/test/java/org/dstadler/jgitfs/JGitFilesystemTest.java
@@ -623,8 +623,15 @@ public class JGitFilesystemTest {
         buffer = ByteBuffer.allocate(1000);
         assertEquals(0, fs.readlink(commit + "/fuse-jna", buffer, 1000));
         assertEquals("Incorrect number of bytes written to the buffer", 924, buffer.remaining());
-        assertEquals("../../../submodule/fuse-jna/commit/a3/1cbf71e03f3f68647794264cc5148d2ed5b1a9",
-				new String(buffer.array(), 0, buffer.position()));
+
+        try {
+			assertEquals("../../../submodule/fuse-jna/commit/ec/ed84f81ad6d2547d47384809a789cf9f4ed3d7",
+					new String(buffer.array(), 0, buffer.position()));
+		} catch (AssertionError e) {
+			// try with a second commit-id that we see locally
+			assertEquals("../../../submodule/fuse-jna/commit/a3/1cbf71e03f3f68647794264cc5148d2ed5b1a9",
+					new String(buffer.array(), 0, buffer.position()));
+		}
 
         StatWrapper stat = getStatsWrapper();
 		assertNotNull(stat);
